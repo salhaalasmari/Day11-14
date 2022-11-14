9b2e6364fe5025446015525dde10048059c9fa1f
salhaalasmari <108153399+salhaalasmari@users.noreply.github.com>
2022-11-14 23:13:33 +0300

    Add files via upload

---
+++ AzizhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azizh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzizhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
